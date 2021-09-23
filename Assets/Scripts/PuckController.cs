59e95b162675af885e2f9df82cafaec80a288330
Makkusu-Tieu <23tieuk@live.elida.k12.oh.us>
2021-09-23 10:03:19 -0400

    Giving Random Direction to Pucks

---
+++ Assets/Scripts/PuckController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuckController : MonoBehaviour
+{
+    public int[] direction = { 0, 1, 2, 3 };
+    public int moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveDirection = direction[Random.Range(0,4)];
+        Debug.Log("moveDirection = " + moveDirection);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
