c4821bd03b67ce2a4a6dfeb283ffef2894ebd8f9
Makkusu-Tieu <23tieuk@live.elida.k12.oh.us>
2021-09-23 09:42:35 -0400

    A lot of stuff

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float xRange;
+    public float yRange;
+    public GameObject Puck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector2(xRange, transform.position.y);
+        }
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector2(-xRange, transform.position.y);
+        }
+
+        if (transform.position.y > yRange)
+        {
+            transform.position = new Vector2(transform.position.x, yRange);
+        }
+        
+        if (transform.position.y < -yRange)
+        {
+            transform.position = new Vector2(transform.position.x, -yRange);
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(Puck, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
+
+        float moveHorizontal = (Input.GetAxis("Horizontal"));
+
+        float moveVertical = (Input.GetAxis("Vertical"));
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
