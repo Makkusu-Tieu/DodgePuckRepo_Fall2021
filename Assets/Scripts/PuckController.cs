a47a689d3aa751d91ac583ea37a693dbc573fc5f
Makkusu-Tieu <23tieuk@live.elida.k12.oh.us>
2021-09-24 10:04:19 -0400

    Puck Movement (Incomplete Wall)

--- Assets/Scripts/PuckController.cs
+++ Assets/Scripts/PuckController.cs
@@ -6,6 +6,7 @@ public class PuckController : MonoBehaviour
 {
     public int[] direction = { 0, 1, 2, 3 };
     public int moveDirection;
+    public float puckSpeed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,29 @@ public class PuckController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(moveDirection == 0)
+        {
+            transform.Translate(transform.up * puckSpeed * Time.deltaTime);
+        }
+
+        if (moveDirection == 1)
+        {
+            transform.Translate(transform.right * puckSpeed * Time.deltaTime);
+        }
+
+        if (moveDirection == 2)
+        {
+            transform.Translate(transform.up * -puckSpeed * Time.deltaTime);
+        }
+
+        if (moveDirection == 3)
+        {
+            transform.Translate(transform.right * -puckSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("WALLLLL");
     }
 }
